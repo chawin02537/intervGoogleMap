1770ede422a851e2fc324f78d98df7d8adf7567d
chawin.kasiwong <chawin.kasiwong@gmail.com>
2022-11-13 02:15:12 +0700

    fix curl text search

--- app/Http/Controllers/GoogleMapController.php
+++ app/Http/Controllers/GoogleMapController.php
@@ -5,48 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Config;
 
+use function PHPSTORM_META\type;
+
 class GoogleMapController extends Controller
 {
-    public function map()
+    public string $googleKey;
+
+    public function __construct()
     {
-        return view('googlemap.index');
+        $this->googleKey = \Config::get('googleapi.GOOGLE_MAP_KEY');
     }
 
-    public function getLocationFromText(Request $request)
+    public function map()
     {
-        $curl = curl_init();
-
-        $searchName = $request->name;
-        // replace spaces with + sign
-        $searchName = str_replace(' ', '+', $searchName);
-
-        $circle = 100;
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=' . $searchName . '&inputtype=textquery&fields=geometry,formatted_address&locationbias=point:50,10&key=' . env('GOOGLE_MAP_KEY'),
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
-                'Accept: application/json'
-            ),
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-
-        // get lat and lng
-        $data = json_decode($response, true);
-        $lat = $data['candidates'][0]['geometry']['location']['lat'];
-        $lng = $data['candidates'][0]['geometry']['location']['lng'];
-
-        // dd($request->name, $lat, $lng);
-
         return view('googlemap.index');
     }
 
@@ -65,16 +36,198 @@ class GoogleMapController extends Controller
     - more beautiful
     */
 
-    public function getNearbyPlaces()
+    public function getNearbyPlaces(Request $request)
+    {
+        $lat = 13.828253;
+        $lng = 100.567481;
+
+        if ($request->ajax()) {
+            $searchName = $request->searchName;
+
+            // if empty search name
+            if (empty($searchName)) {
+                $result = $this->googleNearbySearch($lat, $lng, $searchName);
+            } else {
+                $result[] = $this->googleTextSearch($lat, $lng, $searchName);
+            }
+            return response()->json($result);
+        }
+
+        $result = '';
+        $count = 0;
+
+        return view('googlemap.nearby', compact('result', 'count'));
+    }
+
+    protected function googleNearbySearch($lat, $lng, $searchName)
+    {
+        try {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' . $lat . '%2C' . $lng . '&radius=1500&type=restaurant&key=' . $this->googleKey,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 100,
+                CURLOPT_TIMEOUT => 10,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => array(
+                    'Accept: application/json'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+
+        $response = json_decode($response, true);
+
+        $token = isset($response['next_page_token']) ? $response['next_page_token'] : '';
+
+        $dataForResponse = [];
+
+        foreach ($response['results'] as $key => $value) {
+            // if status == OPERATIONAL
+            if ($value['business_status'] == 'OPERATIONAL') {
+                $dataForResponse[] = [
+                    'name' => $value['name'],
+                    'lat' => $value['geometry']['location']['lat'],
+                    'lng' => $value['geometry']['location']['lng'],
+                    'place_id' => $value['place_id'],
+                ];
+            }
+        }
+
+        checkToken:
+
+        $dataCount = count($dataForResponse);
+
+        if (!empty($token) && $dataCount < 5) {
+
+            sleep(3);
+
+            try {
+                $curl = curl_init();
+
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=' . $token . '&key=' . $this->googleKey,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 100,
+                    CURLOPT_TIMEOUT => 10,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'GET',
+                    CURLOPT_HTTPHEADER => array(
+                        'Accept: application/json'
+                    ),
+                ));
+
+                $response = curl_exec($curl);
+
+                curl_close($curl);
+            } catch (\Exception $e) {
+                return $dataForResponse;
+            }
+
+            $response = json_decode($response, true);
+
+            $token = isset($response['next_page_token']) ? $response['next_page_token'] : '';
+
+            foreach ($response['results'] as $key => $value) {
+                if ($value['business_status'] == 'OPERATIONAL') {
+                    $dataForResponse[] = [
+                        'name' => $value['name'],
+                        'lat' => $value['geometry']['location']['lat'],
+                        'lng' => $value['geometry']['location']['lng'],
+                        'place_id' => $value['place_id'],
+                    ];
+                }
+            }
+            goto checkToken;
+        }
+
+        // foreach get place detail
+        foreach ($dataForResponse as $key => $value) {
+
+            $placeId = $value['place_id'];
+
+            $response = $this->getPlaceInfo($placeId);
+
+            // add url, adr_address
+            $dataForResponse[$key]['url'] = $response['result']['url'];
+            $dataForResponse[$key]['adr_address'] = $response['result']['adr_address'];
+        }
+
+        return $dataForResponse;
+    }
+
+    protected function googleTextSearch($lat, $lng, $searchName)
+    {
+        // url encode search name utf-8
+        $searchName = urlencode($searchName);
+
+        $inputtype = 'textquery';
+
+        $locationbias = 'circle%3A1500%40' . $lat . '%2C' . $lng;
+
+        try {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery&input=' . $searchName . '&locationbias=circle%3A2000%4013.828253%2C100.567481&key=AIzaSyCbqwITBpnjJmjMF6GZmKFV1CgktxlC2Hw',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => array(
+                    'Accept: application/json'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            $response = json_decode($response, true);
+
+            $place_id = isset($response['candidates'][0]['place_id']) ? $response['candidates'][0]['place_id'] : '';
+
+            $response = $this->getPlaceInfo($place_id);
+
+            $dataForResponse = [
+                'name' => $response['result']['name'],
+                'lat' => $response['result']['geometry']['location']['lat'],
+                'lng' => $response['result']['geometry']['location']['lng'],
+                'place_id' => $response['result']['place_id'],
+                'url' => $response['result']['url'],
+                'adr_address' => $response['result']['adr_address'],
+            ];
+
+            return $dataForResponse;
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    protected function getPlaceInfo($placeId)
     {
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=13.828253%2C100.5284507&radius=100&type=restaurant&key=' . env('GOOGLE_MAP_KEY'),
+            CURLOPT_URL => 'https://maps.googleapis.com/maps/api/place/details/json?place_id=' . $placeId . '&key=' . $this->googleKey,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 100,
-            CURLOPT_TIMEOUT => 0,
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 5,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'GET',
@@ -87,29 +240,8 @@ class GoogleMapController extends Controller
 
         curl_close($curl);
 
-        $dataForSend = [];
+        $response = json_decode($response, true);
 
-        // get name and lat and lng
-        $data = json_decode($response, true);
-
-        $count = 1;
-
-        foreach ($data['results'] as $key => $value) {
-            $name = $value['name'];
-            $lat = $value['geometry']['location']['lat'];
-            $lng = $value['geometry']['location']['lng'];
-            // add to data array
-            $dataForSend[] = [
-                'name' => $name,
-                'lat' => $lat,
-                'lng' => $lng,
-                'count' => $count
-            ];
-
-            $count++;
-        }
-
-        // dd($dataForSend);
-        return view('googlemap.nearby', compact('dataForSend'));
+        return $response;
     }
 }
--- resources/views/googlemap/nearby.blade.php
+++ resources/views/googlemap/nearby.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>How to Add Google Map in Laravel? - ItSolutionStuff.com</title>
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style type="text/css">
         #map {
             height: 800px;
@@ -14,69 +16,121 @@
 </head>
 
 <body>
+    {{-- form input searchName with button submit_search --}}
+    <div class="container form mb-3">
+        <div class="row d-flex justify-content-center">
+            <input class="form-control mb-2" type="text" id="searchName" name="searchName" placeholder="Search Name">
+            <button type="button" class="col-6 btn btn-primary" id="submit_search" name="submit_search">Search</button>
+        </div>
+    </div>
 
     <div class="container">
         <div id="map"></div>
     </div>
 
-    <script type="text/javascript">
-        function initMap() {
-            var markers = [];
-
-            // foreach dataForSend add name lat lng to markers
-            @foreach($dataForSend as $value)
-            var name = '{{$value['name']}}';
-            var lat = {{$value['lat']}};
-            var lng = {{$value['lng']}};
-            var count = {{$value['count']}};
-
-            // make array
-            markers.push([name, lat, lng, count]);
-
-            @endforeach
-
-            console.log(markers);
-            console.log(markers.length);
-
-            const map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 20,
-                center: new google.maps.LatLng(13.828253, 100.5284507),
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            });
-
-            // foreach markers add marker
-            for (let i = 1; i <= markers.length; i++) {
-                
-                var name = markers[i][0];
-                var lat = markers[i][1];
-                var lng = markers[i][2];
-                var count = markers[i-1][3];
-                console.log(i, count);
-
-                var myLatLng = new google.maps.LatLng(lat, lng);
-                var marker = new google.maps.Marker({
-                    position: myLatLng,
-                    map: map,
-                    title: name
-                });
-
-                var infowindow = new google.maps.InfoWindow({
-                    content: name + ' ' + count
-                });
-
-                marker.addListener('click', function() {
-                    infowindow.open(map, marker);
-                });
-            }
-        }
-
-        window.initMap = initMap;
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
 
     <script type="text/javascript"
         src="https://maps.google.com/maps/api/js?key={{ env('GOOGLE_MAP_KEY') }}&callback=initMap">
     </script>
 
+    <script type="text/javascript">
+        $(document).ready(function() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: new google.maps.LatLng(13.828253, 100.567481),
+                zoom: 15
+            });
+            window.initMap = initMap;
+            initMap('');
+        });
+        
+        $(function () {
+            document.getElementById("submit_search").onclick = function () {
+                var searchName = $("#searchName").val();
+                deleteMarkers();
+                initMap(searchName);
+            }
+        })
+
+        var markers = [];
+        var lat = 13.828253;
+        var lng = 100.567481;
+
+        function initMap(searchName) {
+            // request get data from GoogleMapController
+            $.ajax({
+                url: "{{ route('map.nearby') }}",
+                type: "GET",
+                data: {
+                    searchName: searchName
+                },
+                success: function (data) {
+
+                    var totalCount = data.length;
+                    var count = totalCount;
+
+                    var infowindow = null;
+                    
+                    $.each(data, function (index, value) {
+                        var name = value.name;
+                        var lat = value.lat;
+                        var lng = value.lng;
+                        var count = totalCount - index;
+                        var url = value.url;
+                        var adr = value.adr;
+
+                        count = count - 1;
+
+                        var contentString = 
+                        '<div id="content">'+
+                        '<div id="siteNotice">'+
+                        '</div>'+
+                        '<h1 id="firstHeading" class="firstHeading">'+name+'</h1>'+
+                        '<div id="bodyContent">'+
+                        '<p><b>'+name+'</b>, '+adr+'</p>'+
+                        '<p><a href="'+url+'">'+
+                        'Go to Map</a> '+
+                        '</div>'+
+                        '</div>';
+                        
+                        // set marker
+                        var marker = new google.maps.Marker({
+                            position: new google.maps.LatLng(lat, lng),
+                            map: map,
+                            title: name,
+                        });
+
+                        // add event listener
+                        google.maps.event.addListener(marker, 'click', (function (marker, count) {
+                            return function () {
+                                if (infowindow) {
+                                    infowindow.close();
+                                }
+                                infowindow = new google.maps.InfoWindow({
+                                    content: contentString
+                                });
+                                infowindow.open(map, marker);
+                            }
+                        })(marker, count));
+
+                        markers.push(marker);
+                    });
+                        
+                    window.initMap = initMap;
+                }
+            });
+        }
+
+        function deleteMarkers() {
+            for (var i = 0; i < markers.length; i++) {
+                markers[i].setMap(null);
+            }
+            markers = [];
+        }
+        
+    </script>
 </body>
 
 </html>
